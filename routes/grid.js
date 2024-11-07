49cf8137f25227661a51d87f8bf97a9a5b92b879
bmwolveriney <s573066@nwmissouri.edu>
2024-11-06 20:55:13 -0600

    adding grid files

--- routes/grid.js
+++ routes/grid.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET grid page. */
 router.get('/', function(req, res, next) {
   let query = req.query;
-  console.log(`rows ${query.rows}`);
-  console.log(`cols ${query.cols}`);
+  console.log(`rows: ${query.rows}`);
+  console.log(`cols: ${query.cols}`);
   
   res.render('grid', { title: 'Grid Display', query: query });
 });
